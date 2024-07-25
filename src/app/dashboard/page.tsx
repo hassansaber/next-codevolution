184b198117f682b61e9eee8b581978c94a7f2759
hassansaber <hassansaberkarin@gmail.com>
2024-07-25 21:02:21 +0330

    15 done

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export function Name() {
+  return <h1>name</h1>;
+}
+
+export default function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,13 @@
+export default function ProductDetailLayout({
+  children,
+}: {
+  children: React.ReactNode,
+}) {
+  return (
+    <>
+      {children}
+      <h2>Featured product</h2>
+      {/* carousel here */}
+    </>
+  );
+}
