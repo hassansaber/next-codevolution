ae53942ffe537be84b7fb0cf86e83d5d0a489332
Hassan Saber <hassansaberkarin@gmail.com>
2024-08-04 06:04:38 +0330

    25 done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
-export function Name() {
-  return <h1>name</h1>;
-}
+// export function Name() {
+//   return <h1>name</h1>;
+// }
 
 export default function Dashboard() {
   return <h1>Dashboard</h1>;
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
 import { notFound } from "next/navigation";
+
+function getRandomInt(counter: number) {
+  return Math.floor(Math.random() * counter);
+}
 
 export default function ReviewDetail({
   params,
@@ -8,6 +14,11 @@ export default function ReviewDetail({
     reviewId: string;
   };
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1)
+    throw new Error("Something went wrong loading review");
+
   if (parseInt(params.reviewId) > 100) notFound();
 
   return (
